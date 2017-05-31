d01459e4234b89da2cec0eea4e38cd96d8faba30
ken90242 <ken90242@gmail.com>
2017-05-31 14:19:36 +0800

    'fix_7'

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express')
 var app = express()
 
 app.get('/',function(req,res){
-	res.sendFile("index.html")
+	res.sendFile("./index.html")
 })
 var port = process.env.PORT || 3000
 app.listen(port,function(){
