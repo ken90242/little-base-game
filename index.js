3e394333b36bd5611f928890cb2a1920918a0ae7
ken90242 <ken90242@gmail.com>
2017-05-31 14:15:57 +0800

    'fix_5'

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var app = express()
 app.use('/',function(req,res){
 	res.sendFile("index.html")
 })
-var port = process.env.PORT || 3000
+var port = 3000
 app.listen(port,function(){
 	console.log('listening on port:'+port)
 })
